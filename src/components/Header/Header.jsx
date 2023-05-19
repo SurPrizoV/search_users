4f5330faa794dfdb0c01ec276ec4116cf6d686b1
Roman <aromawriter@mail.ru>
2023-05-19 18:46:01 +0300

    pagination

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import s from "./Header.module.css";
 import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { Pagination } from "../Pagination/Pagination";
 
 export const Header = ({ responseData, setResponseData }) => {
   const [value, setValue] = useState("");
   const [desc, setDesc] = useState("desc");
-  const [pageNumber, setPageNumber] = useState("1");
+  const [page, setPage] = useState(1);
 
   const fetchData = async (url) => {
     try {
@@ -34,7 +33,7 @@ export const Header = ({ responseData, setResponseData }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const myLink = `https://api.github.com/search/users?q=${value}`;
+    const myLink = `https://api.github.com/search/users?q=${value}&per_page=48&page=${page}`;
     const responseData = await fetchData(myLink);
     setResponseData(responseData);
   };
@@ -42,16 +41,15 @@ export const Header = ({ responseData, setResponseData }) => {
   const handleFilter = async (event) => {
     event.preventDefault();
     desc === "desc" ? setDesc("asc") : setDesc("desc");
-    const filterLink = `https://api.github.com/search/users?q=${value}&sort=repositories&order=${desc}`;
+    const filterLink = `https://api.github.com/search/users?q=${value}&per_page=48&page=${page}&sort=repositories&order=${desc}`;
     const responseData = await fetchData(filterLink);
     setResponseData(responseData);
   };
 
-  const handlePagination = async (event, page) => {
-    event.preventDefault();
-    setPageNumber(page);
-    const paginationLink = `https://api.github.com/search/users?q=${value}&per_page=48&page=${page}`;
-    const responseData = await fetchData(paginationLink);
+  const handlePageChange = async (newPage) => {
+    setPage(newPage);
+    const myLink = `https://api.github.com/search/users?q=${value}&per_page=48&page=${newPage}`;
+    const responseData = await fetchData(myLink);
     setResponseData(responseData);
   };
 
@@ -76,11 +74,18 @@ export const Header = ({ responseData, setResponseData }) => {
           ? "Сортировать по возрастанию"
           : "Сортировать по убыванию"}
       </button>
-      <Pagination
-        responseData={responseData}
-        pageNumber={pageNumber}
-        handlePagination={handlePagination}
-      />
+      <div className={responseData ? `${s.pagination}` : `${s.hidden}`}>
+        <button
+          className={s.button_filter}
+          onClick={() => handlePageChange(page - 1)}>
+          Предыдущая
+        </button>
+        <button
+          className={s.button_filter}
+          onClick={() => handlePageChange(page + 1)}>
+          Следующая
+        </button>
+      </div>
     </>
   );
 };
